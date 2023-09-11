a1efde20f466da2c0ca7596fed0acd16634f3dcb
krisjanis-salenieks <144595869+krisjanis-salenieks@users.noreply.github.com>
2023-09-11 22:01:37 +0300

    Add files via upload

---
+++ result.py
@@ -0,0 +1,18 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? ").rstrip())
+    percent = percent_to_float(input("What percentage would you like to tip? ").rstrip())
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+def dollars_to_float(d):
+    dollFloat = float(d.replace("$","").strip())
+    return (dollFloat)
+    pass
+        
+
+def percent_to_float(p):
+    percFloat = (float(p.replace("%","").strip()))/100
+    return (percFloat)
+    pass
+
+main()
